3c9c553e46f544f625b9b69820c360f556d5c5b6
Tayeb Zaidi <tayebzaidi@gmail.com>
2024-03-12 14:13:59 -0500

    Update to implement parallel implementation with complete graph

--- PythonPackageTayeb/dgd_helpers.py
+++ PythonPackageTayeb/dgd_helpers.py
@@ -1,5 +1,25 @@
-def load_data_for_node(rank,size):
-    return 1
+import scipy as sp
+
+def load_data_for_node(comm, rank, size):
+    print(rank)
+
+    data_to_process = None
+
+    if rank == 0:
+        data = sp.io.loadmat('../Experimentation/full_align_L6_q3(1).mat')
+        sequence_data = data["full_align"] # numpy array of sequences
+        sequence_data -= 1; # to account for python vs. matlab starting integers
+    
+        ##Apportion correct amounts of data to each node
+        total_seqs = 1024
+        seq_per_node = total_seqs // size # Assume evenly divisible (power of twos only)
+        data_chunks = [sequence_data[i* seq_per_node:(i+1) * seq_per_node] for i in range(size)]
+        print(data_chunks)
+    else:
+        data_chunks = None
+    
+    data_subsection = comm.scatter(data_chunks, root=0)
+    return data_subsection
 
 def initialize_model_parameters():
     return 1
